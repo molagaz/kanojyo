8803d5a1062838c66ad17a00cc31c9d90c98f819
311518 <311518@nd.com>
2018-04-08 19:17:41 +0800

    fallen->been by kanojyo.

--- days.js
+++ days.js
@@ -8,6 +8,6 @@ function timeafter(spec){return Math.floor((Date.now() - spec)/1000/3600/24);}
   var wefil = timeafter(day2);
  
   var style = 'font-size:3em; font-family:"微软雅黑"; color:#EA9DAA; text-shadow: -1px 1px 1px #FFBCC8A2, -2px 2px 1px #FFBCC891, -3px 3px 1px #FFBCC880, -4px 4px 1px #FFBCC86F, -5px 5px 1px #FFBCC85E, -6px 6px 1px #FFBCC84D, -7px 7px 1px #FFBCC83C, -8px 8px 1px #FFBCC82B, -9px 9px 1px #FFBCC81A';	
-  console.log('%c we\'ve already met for about '+wemet+' days, and fallen in love for '+wefil+' days, my darling.', style); 
+  console.log('%c we\'ve already met for about '+wemet+' days, and been in love for '+wefil+' days, my darling.', style); 
 })();
 
